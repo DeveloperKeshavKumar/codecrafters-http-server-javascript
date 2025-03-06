af1534313bda5f61d55805ae00154b3d4619cdcb
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-03-07 01:58:19 +0530

    add http header

--- app/main.js
+++ app/main.js
@@ -5,10 +5,10 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
     socket.write('HTTP/1.1 200 OK\r\n\r\n');
-    socket.end();
-  });
+    socket.on("close", () => {
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
