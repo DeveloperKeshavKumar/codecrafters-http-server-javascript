373909a4059829c93953a6d1140fd55c12b11352
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-04-13 17:59:08 +0530

    respond with body

--- app/main.js
+++ app/main.js
@@ -8,12 +8,16 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
         const headers = request.split('\r\n');
+        const idx = headers[0].split(' ');
+        const route = idx[1];
+        
+        socket.write(`HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${route.length -1}\r\n\r\n${route}`)
 
-        if (headers[0] === 'GET / HTTP/1.1') {
-            socket.write('HTTP/1.1 200 OK\r\n\r\n')
-        } else {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
-        }
+        // if (headers[0] === 'GET / HTTP/1.1') {
+        //     socket.write('HTTP/1.1 200 OK\r\n\r\n')
+        // } else {
+        //     socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+        // }
     });
 
     socket.on("close", () => {
