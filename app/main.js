40389698aed2aa777ab3f0113118913ad4529db5
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-04-13 18:27:26 +0530

    try#1: read the header

--- app/main.js
+++ app/main.js
@@ -8,8 +8,10 @@ const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
         const headers = request.split('\r\n');
-        const idx = headers[0].split(' ');
-        let route = idx[1];
+        let statusLine = headers[0].split(' ');
+        let hostLine = headers[1];
+        let requestHeaders = headers[2];
+        let route = statusLine[1];
 
         // Respond With 200
         // socket.write('HTTP/1.1 200 OK\r\n\r\n');
@@ -25,15 +27,19 @@ const server = net.createServer((socket) => {
         // }
 
         // Respond With Body
-        if (route.startsWith('/echo/')) {
-            route = route.slice(6);
-            socket.write(`HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${route.length}\r\n\r\n${route}`);
-        } else if (route === '/') {
-            socket.write('HTTP/1.1 200 OK\r\n\r\n');
-        }
-        else {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-        }
+        // if (route.startsWith('/echo/')) {
+        //     route = route.slice(6);
+        //     socket.write(`HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${route.length}\r\n\r\n${route}`);
+        // } else if (route === '/') {
+        //     socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        // }
+        // else {
+        //     socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+        // }
+
+        // Read Header
+        const userAgent = requestHeaders.split(': ')[1];
+        socket.write(`HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${userAgent.length}\r\n\r\n${userAgent}`)
     });
 
     socket.on("close", () => {
