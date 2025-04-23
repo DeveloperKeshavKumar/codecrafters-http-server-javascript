ac15cc5a363c89a102626d4ce10e299d512f6265
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-04-24 02:02:26 +0530

    done Read request body

--- app/main.js
+++ app/main.js
@@ -2,62 +2,96 @@ const net = require("net");
 const fs = require("fs");
 const path = require("path");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        const request = data.toString();
-        const headers = request.split('\r\n');
-        // console.log(headers);
+    socket.on("data", (data) => {
+        const rawRequest = data.toString();
+        const [headerPart, body] = rawRequest.split('\r\n\r\n');
 
-        let statusLine = headers[0].split(' ');
-        let route = statusLine[1];
+        const requestLines = headerPart.split('\r\n');
+        const requestLine = requestLines[0];
+        const [method, urlPath, httpVersion] = requestLine.split(' ');
 
-        // Return A File
-        if (route.startsWith('/files/')) {
-            const requestedFile = route.slice(7);
+        const headerLines = requestLines.slice(1).filter(line => line.length > 0);
+
+        const headers = {};
+        for (const line of headerLines) {
+            const [key, value] = line.split(': ');
+            headers[key] = value;
+        }
+
+        if (urlPath.startsWith('/files/')) {
             const directoryIndex = process.argv.indexOf('--directory');
             const baseDir = directoryIndex !== -1 ? process.argv[directoryIndex + 1] : path.join(__dirname, 'tmp');
-            const filePath = path.join(baseDir, requestedFile);
+            if (method === 'GET') {
+                const requestedFile = urlPath.slice(7);
+                const filePath = path.join(baseDir, requestedFile);
 
-            fs.readFile(filePath, (err, data) => {
-                if (err) {
-                    socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-                    return;
+                fs.readFile(filePath, (err, fileData) => {
+                    if (err) {
+                        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+                        socket.end();
+                        return;
+                    }
+
+                    socket.write(
+                        `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileData.length}\r\n\r\n`
+                    );
+                    socket.write(fileData);
+                    socket.end();
+                });
+            } else if (method === 'POST') {
+                const requestedFile = urlPath.slice(7);
+                const filePath = path.join(baseDir, requestedFile);
+
+                if (!fs.existsSync(baseDir)) {
+                    fs.mkdirSync(baseDir, { recursive: true });
                 }
 
-                socket.write(
-                    `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${data.length}\r\n\r\n`
-                );
-                socket.write(data);
-            });
+                fs.writeFile(filePath, body, (err) => {
+                    if (err) {
+                        console.error("File write error:", err);
+                        socket.write('HTTP/1.1 500 Internal Server Error\r\n\r\n');
+                    } else {
+                        socket.write('HTTP/1.1 201 Created\r\n\r\n');
+                    }
+                    socket.end();
+                });
+            }
 
             return;
         }
-        // Read Header
-        else if (route === '/user-agent') {
-            const userAgentLine = headers.filter(x => x.startsWith("User-Agent:"));
-            const userAgent = userAgentLine[0].split(": ")[1];
+
+        else if (urlPath === '/user-agent') {
+            const userAgent = headers['User-Agent'];
             if (userAgent !== undefined) {
-                socket.write(`HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${userAgent.length}\r\n\r\n${userAgent}`)
+                socket.write(
+                    `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+                );
+            } else {
+                socket.write('HTTP/1.1 400 Bad Request\r\n\r\n');
             }
-        }
-        // Respond With Body
-        else if (route.startsWith('/echo/')) {
-            route = route.slice(6);
-            socket.write(`HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${route.length}\r\n\r\n${route}`);
-        }
-        // Respond With 200
-        else if (route === '/') {
-            socket.write('HTTP/1.1 200 OK\r\n\r\n');
-        }
-        // Extract URL Path
-        else {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+            socket.end();
+            return;
         }
 
+        else if (urlPath.startsWith('/echo/')) {
+            const message = urlPath.slice(6);
+            socket.write(
+                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${message.length}\r\n\r\n${message}`
+            );
+            socket.end();
+            return;
+        }
+
+        else if (urlPath === '/') {
+            socket.write('HTTP/1.1 200 OK\r\n\r\n');
+            socket.end();
+            return;
+        }
+
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
         socket.end();
     });
 
